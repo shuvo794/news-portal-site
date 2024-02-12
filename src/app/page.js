827c0cba1e58c6b484df6165e83ddba2fdf43a0d
shuvo794 <shuvohossain794@gmail.com>
2024-02-12 09:19:02 +0600

    create falder

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import React from 'react';
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
+    <div>
+      
+    </div>
   );
-}
+};
+
+export default HomePage;
