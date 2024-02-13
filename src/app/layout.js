556609be5c09d0e628126d64fa2f2aa3459aeb72
shuvo794 <shuvohossain794@gmail.com>
2024-02-13 11:23:51 +0600

    work  footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar/Navbar";
 import Footer from "@/components/shared/Footer/Footer";
+import { Container } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
+        <Navbar />
+        <Container className="min-h-screen">{children}</Container>
+        <Footer />
       </body>
     </html>
   );
