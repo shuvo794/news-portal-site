dc64e78dabeea827e1b2ab457d5398438315d5ef
shuvo794 <shuvohossain794@gmail.com>
2024-02-13 11:12:15 +0600

    work  footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import { Box, Container } from '@mui/material';
+
+import { IconButton, Stack } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+      <Box className="bg-black px-2 py-10">
+        <Container>
+                <Box
+                    className=""
+            sx={{
+              "& svg": {
+                color: "white",
+              },
+            }}
+          >
+           
+              <IconButton>
+                <FacebookIcon />
+              </IconButton>
+              <IconButton>
+                <GoogleIcon />
+              </IconButton>
+              <IconButton>
+                <LinkedInIcon />
+              </IconButton>
+              <IconButton>
+                <TelegramIcon />
+              </IconButton>
+          
+          </Box>
+        </Container>
+      </Box>
     );
 };
 
