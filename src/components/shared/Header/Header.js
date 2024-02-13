0e0d3863144d8abdd2e0f3e4f81a28a53e623a29
shuvo794 <shuvohossain794@gmail.com>
2024-02-13 18:56:55 +0600

    send

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,14 +1,25 @@
 import Image from 'next/image';
 import headerLogo from '../../../assets/Logo (2).png'
 import { Box, Typography } from '@mui/material';
+import { displayCurrentDate } from '@/utils/displayCurrentDate';
 const Header = () => {
+  const currentDate = displayCurrentDate();
     return (
-        <Box>
-            <Image className='mx-auto' src={headerLogo} width={200} height={100} alt='logo' />
-            <Typography variant='body1' color='gray' align='center' mt='5'>
-             All Jurnalist Write articel here
-            </Typography>
-        </Box>
+      <Box>
+        <Image
+          className="mx-auto"
+          src={headerLogo}
+          width={150}
+          height={100}
+          alt="logo"
+        />
+        <Typography variant="body1" color="gray" align="center">
+          All Jurnalist Write articel here
+        </Typography>
+        <Typography variant="body1" color="gray" align="center">
+          {currentDate}
+        </Typography>
+      </Box>
     );
 };
 
