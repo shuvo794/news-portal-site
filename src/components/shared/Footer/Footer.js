350b8a486c5731495d1f73044eafa39a8bc8d551
shuvo794 <shuvohossain794@gmail.com>
2024-02-13 11:15:05 +0600

    work  footer

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,37 +1,71 @@
-import { Box, Container } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
 
 import { IconButton, Stack } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import GoogleIcon from "@mui/icons-material/Google";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TelegramIcon from "@mui/icons-material/Telegram";
+import Link from 'next/link';
+
+const navItems = [
+  {
+    route: "Home",
+    pathName: "/",
+  },
+  {
+    route: "Pages",
+    pathName: "/pages",
+  },
+  {
+    route: "Category",
+    pathName: "/category",
+  },
+  {
+    route: "News",
+    pathName: "/news",
+  },
+  {
+    route: "About",
+    pathName: "/about",
+  },
+  {
+    route: "Contuct",
+    pathName: "/contuct",
+  },
+];
 
 const Footer = () => {
     return (
       <Box className="bg-black px-2 py-10">
         <Container>
-                <Box
-                    className="w-full text-center"
+          <Box
+            className="w-full text-center"
             sx={{
               "& svg": {
                 color: "white",
               },
             }}
           >
-           
-              <IconButton>
-                <FacebookIcon />
-              </IconButton>
-              <IconButton>
-                <GoogleIcon />
-              </IconButton>
-              <IconButton>
-                <LinkedInIcon />
-              </IconButton>
-              <IconButton>
-                <TelegramIcon />
-              </IconButton>
-          
+            <IconButton>
+              <FacebookIcon />
+            </IconButton>
+            <IconButton>
+              <GoogleIcon />
+            </IconButton>
+            <IconButton>
+              <LinkedInIcon />
+            </IconButton>
+            <IconButton>
+              <TelegramIcon />
+            </IconButton>
+          </Box>
+
+          <Box className="text-center w-full">
+            {navItems.map((item) => (
+              <Link key={item} href={item.pathName}>
+                <Button className="text-white">{item.route}</Button>
+              </Link>
+            ))}
           </Box>
         </Container>
       </Box>
