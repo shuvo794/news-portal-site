4b5ee44a6581f91f82247c3a38d1002582fe86df
shuvo794 <shuvohossain794@gmail.com>
2024-02-15 19:24:18 +0600

    work a sidebar news section

--- src/components/ui/SideBar/SideBar.js
+++ src/components/ui/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, ButtonBase, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import Rectangle from "../../../assets/side-top-news.png";
 
@@ -11,7 +11,7 @@ const SideBar = () => {
               <Image src={Rectangle} width={800} alt="HeroImage" />
             </CardMedia>
             <Typography>
-              <p className="bg-red-500  rounded text-white my-4 p-1">
+              <p className="bg-[#192a56]  rounded text-white my-4 p-1">
                 <marquee> The Technology is new of the world</marquee>
               </p>
             </Typography>
@@ -25,6 +25,27 @@ const SideBar = () => {
             </CardContent>
           </CardActionArea>
         </Card>
+        <Box>
+          <Grid container spacing={2}>
+            <Grid item>
+              <ButtonBase sx={{ width: 128, height: 128 }}>
+                <Image src={Rectangle} alt="" />
+              </ButtonBase>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid className="my-6" item xs>
+                  <Typography gutterBottom variant="subtitle1" component="div">
+                    Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it
+                  </Typography>
+                  <Typography variant="body2" gutterBottom>
+                    Mar 18 2023{" "}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     );
 };
