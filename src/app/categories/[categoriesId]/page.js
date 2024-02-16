f6a1e5d5b1791bc49ec30209799260d5cf06a8ae
shuvo794 <shuvohossain794@gmail.com>
2024-02-16 10:18:51 +0600

    create getAllNews show the page

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,13 +1,53 @@
 import { getAllNews } from "@/utils/getAllNews";
-import { Box } from "@mui/material";
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import Image from "next/image";
 
 const DynamicNews =async ({params, searchParams }) => {
     // console.log(searchParams);
-    const {data} =await getAllNews(searchParams.category);
+    const {data:getAllDatas} =await getAllNews(searchParams.category);
     // console.log(data);
   return (
     <Box className="mt-5">
-          <h2>Total {searchParams.category} News { data.length}</h2>
+      <h2>
+        Total {searchParams.category} News {getAllDatas.length}
+      </h2>
+      <Grid
+        className="my-5"
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        {getAllDatas.map((getAllData) => (
+          <Grid key={getAllData._id} item xs={6}>
+            <Card>
+              <CardActionArea>
+                <CardMedia>
+                  <Image
+                    src={getAllData.thumbnail_url}
+                    width={800}
+                    height={100}
+                    alt="HeroImage"
+                  />
+                </CardMedia>
+
+                <CardContent>
+                  <Typography gutterBottom component="div">
+                    {getAllData.title}
+                  </Typography>
+                  <Typography gutterBottom component="div">
+                    By {getAllData.author.name} -{" "}
+                    {getAllData.author.published_date}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    It is a long established fact that a reader will be
+                    distracted by the readable.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
