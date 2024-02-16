d8731c2ca113395228e8597ffc8bcbc374ca4094
shuvo794 <shuvohossain794@gmail.com>
2024-02-16 11:19:31 +0600

    create details page

---
+++ src/app/[news]/[newsId]/page.js
@@ -0,0 +1,9 @@
+const NewsDetailsPage = ({params}) => {
+    return (
+        <div>
+            {params.newsId}
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -1,6 +1,7 @@
 import { getAllNews } from "@/utils/getAllNews";
 import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 const DynamicNews =async ({params, searchParams }) => {
     // console.log(searchParams);
@@ -9,7 +10,8 @@ const DynamicNews =async ({params, searchParams }) => {
   return (
     <Box className="mt-5">
       <h2>
-        Total <span className="font-bold">{searchParams.category}</span> News {getAllDatas.length}
+        Total <span className="font-bold">{searchParams.category}</span> News{" "}
+        {getAllDatas.length}
       </h2>
       <Grid
         className="my-5"
@@ -19,42 +21,44 @@ const DynamicNews =async ({params, searchParams }) => {
       >
         {getAllDatas.map((getAllData) => (
           <Grid key={getAllData._id} item xs={6}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  sx={{
-                    "& img": {
-                      width: "100%",
-                      height: "250px",
-                    },
-                  }}
-                >
-                  <Image
-                    src={getAllData.thumbnail_url}
-                    width={800}
-                    height={100}
-                    alt="HeroImage"
-                  />
-                </CardMedia>
+            <Link>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    sx={{
+                      "& img": {
+                        width: "100%",
+                        height: "250px",
+                      },
+                    }}
+                  >
+                    <Image
+                      src={getAllData.thumbnail_url}
+                      width={800}
+                      height={100}
+                      alt="HeroImage"
+                    />
+                  </CardMedia>
 
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {getAllData.title.length > 30
-                      ? getAllData.title.slice(0, 30) + "..."
-                      : getAllData.title}
-                  </Typography>
-                  <Typography gutterBottom component="div">
-                    By {getAllData.author.name} -{" "}
-                    {getAllData.author.published_date}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {getAllData.details.length > 200
-                      ? getAllData.details.slice(0, 200) + " ... "
-                      : getAllData.details}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {getAllData.title.length > 30
+                        ? getAllData.title.slice(0, 30) + "..."
+                        : getAllData.title}
+                    </Typography>
+                    <Typography gutterBottom component="div">
+                      By {getAllData.author.name} -{" "}
+                      {getAllData.author.published_date}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {getAllData.details.length > 200
+                        ? getAllData.details.slice(0, 200) + " ... "
+                        : getAllData.details}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
