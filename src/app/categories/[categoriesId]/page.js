fc719913d8a039119ee4786a4a576b6785de7af8
shuvo794 <shuvohossain794@gmail.com>
2024-02-16 10:47:53 +0600

    added detail

--- src/app/categories/[categoriesId]/page.js
+++ src/app/categories/[categoriesId]/page.js
@@ -39,8 +39,7 @@ const DynamicNews =async ({params, searchParams }) => {
                     {getAllData.author.published_date}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    It is a long established fact that a reader will be
-                    distracted by the readable.
+                    {getAllData.details}
                   </Typography>
                 </CardContent>
               </CardActionArea>
